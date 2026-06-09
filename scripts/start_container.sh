33567f2646909ec1a1f0d9bce80f96d83d769d29
Zeeshan Kanuga <zeeshankanuga@Zeeshans-MacBook-Air.local>
2026-06-09 15:29:01 +0530

    modefying source files

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
   set -e
 
   # Pull the latest Docker image from Docker Hub
-  docker pull trainwithshubham/tws-online-shop-app-demo:latest
+  docker pull zeeshankanuga/onineshop:latest
 
   # Run the Docker image as a container
-  docker run -d -p 5173:5173 trainwithshubham/tws-online-shop-app-demo:latest
+  docker run -d -p 5173:5173 zeeshankanuga/onineshop:latest
